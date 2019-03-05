ea0b7588e4909dc8cb825b4b9bc20f58807766c7
Kevin Bingham <kevin@jbingham.net>
2019-03-05 11:12:32 -0800

    JSON Read and Write

--- JSON/src/JSONexample/Main.java
+++ JSON/src/JSONexample/Main.java
@@ -9,14 +9,14 @@ public class Main {
         Scanner in = new Scanner(System.in);
 
         while(true) {
-            System.out.print("Interact with JSON (Enter '1' Run or '2' to Quit) - ");
+            System.out.print("\nInteract with JSON (Enter '1' Run or '2' to Quit) - ");
             Integer runView = in.nextInt();
             if (runView == 1) {
                 view.runProgram();
             } else {
                 break;
             }
-            System.out.println("Program ended.");
         }
+        System.out.println("Program ended.");
     }
 }
